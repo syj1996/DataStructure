e8fbd6f523a5b6b951cd08cc388620f83b0a9436
syj1996 <2768730013@qq.com>
2019-06-21 10:30:59 +0800

    数据结构图的遍历深度搜索与广度搜索以及最小生树的生成

--- src/Sort/sort.java
+++ src/Sort/sort.java
@@ -188,11 +188,12 @@ public class sort {
     /**
      * 希尔排序
      *
-     * 1. 选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；（一般初次取数组半长，之后每次再减半，直到增量为1）
+     * 1. 选择一个增量序列t1，t2，…，tk，其中ti》tj，tk=1；（一般初次取数组半长，之后每次再减半，直到增量为1）
      * 2. 按增量序列个数k，对序列进行k 趟排序；
      * 3. 每趟排序，根据对应的增量ti，将待排序列分割成若干长度为m 的子序列，分别对各子表进行直接插入排序。
      *    仅增量因子为1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
      * @param arr  待排序数组
+     * @return int[] 返回排序数组
      */
     public static int[] shellSort(int[] arr){
         int gap = arr.length / 2;
@@ -246,6 +247,7 @@ public class sort {
      * ③. 针对所有的元素重复以上的步骤，除了最后一个。
      * ④. 持续每次对越来越少的元素重复上面的步骤①~③，直到没有任何一对数字需要比较。
      * @param arr  待排序数组
+     * @return int[] 返回排序数组
      */
     public static int[] bubbleSort(int[] arr){
         for (int i = arr.length; i > 0; i--) {      //外层循环移动游标
@@ -270,6 +272,7 @@ public class sort {
      * @param arr   待排序数组
      * @param low   左边界
      * @param high  右边界
+     * @return int[] 返回数组
      */
     public static int[] quickSort(int[] arr, int low, int high){
         if(arr.length <= 0) return null;
@@ -302,6 +305,7 @@ public class sort {
      * ②. 重新排序数列，所有比基准值小的元素摆放在基准前面，所有比基准值大的元素摆在基准后面（相同的数可以到任一边）。在这个分区结束之后，该基准就处于数列的中间位置。这个称为分区（partition）操作。
      * ③. 把分区之后两个区间的边界（low和high）压入栈保存，并循环①、②步骤
      * @param arr   待排序数组
+     * @return Object[] 返回排序数组
      */
     public static Object[] quickSortByStack(int[] arr){
         if(arr.length <= 0) return null;
